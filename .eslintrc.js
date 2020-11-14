a4d12969891245eee6c2fec8af0b85190ff41ebc
inam95 <mfihassan99@gmail.com>
2020-11-14 23:00:42 +0530

    Initial project commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["react-app", "plugin:prettier/recommended"],
+
+  plugins: ["prettier"],
+
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface AppProps {}
+
+const App = (props: AppProps) => {
+  return <div>App</div>;
+};
+
+export default App;
